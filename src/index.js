5b0f33709223afe8ed07d4c658a4d7b2ff292f1a
Brishna Bakshev <b.bakshev@gmail.com>
2023-02-07 16:12:21 -0800

    WIP
    Co-authored-by: Sarah Andyshak <sarah.andyshak@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import './css/styles.css';
 
 // Business Logic
 
-function searchDino(paragraph, word) {
+function searchDino(word) {
   let request = new XMLHttpRequest();
-  const url = `https://dinoipsum.com/api/?format=json&words=5&paragraphs=5`;
+  const url = `https://dinoipsum.com/api/?format=json&words=${word}`; // use generics so user can input values
 
   request.addEventListener("loadend", function(){
     const response = JSON.parse(this.responseText);
@@ -41,25 +41,40 @@ function printElement(apiResponse) {
   document.querySelector('#showResponse').innerText = response;
 }
 
-// function printElement(apiResponse) {
-//   let response = '';
-//   apiResponse.forEach(function (paragraph){
-//     response += paragraph.join(' ');
-//   });
-//   document.querySelector('#showResponse').innerText = response;
+// function printGif(apiResponse) {
+//   document.getElementById('showGif').innerHTML = null;
+//   let p = document.getElementById('showGif');
+//   if (apiResponse.data[1]) {
+//     apiResponse.data.forEach (element => {
+//       p.setAttribute('class', 'imageResult');
+//       p.innerHTML =+ `<img src='${element.images.original.url}`;
+//       document.getElementById('showGif').append(p);
+//     });
+//   }
 // }
 
 function printGif(apiResponse) {
-  document.querySelector('#showGif').innerText = ``;
+  let gif;
+  apiResponse.data.forEach(function(element) {
+    gif = document.createElement("img");
+    gif.setAttribute("src", element["images"]["downsized_medium"]["url"]);
+  });
+  document.getElementById('showGif').append(gif);
 }
+
+// function printGif(apiResponse) {
+//   document.querySelector('#showGif').innerText = `dinosaur!`;
+// }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const paragraphInput = document.querySelector('#paragraph').value;
+  // const paragraphInput = document.querySelector('#paragraph').value;
   const wordInput = document.querySelector('#word').value;
-  searchDino(paragraphInput,wordInput);
+  searchDino(wordInput);
+  showDinoGif();
 }
 
 window.addEventListener("load", function() {
   this.document.querySelector('form').addEventListener("submit", handleFormSubmission);
+
 });
